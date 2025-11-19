2b3d599e83972d72ec93ba1ffc97a3f364f4f04f
younf566 <younf566@newschool.edu>
2025-11-19 16:15:13 -0500

    on

--- main.jsx
+++ main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
